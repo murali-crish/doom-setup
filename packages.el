1a6ed4f14973bd2e0c0a89f668f216a6dfecec9d
Murali Krishna Patteboyina <murali.crish@gmail.com>
2022-12-12 16:24:46 +1000

    Add fira code as font including ligatures

--- packages.el
+++ packages.el
@@ -16,6 +16,7 @@
 (package! nov)
 (package! tide)
 (package! prettier-js)
+(package! pdf-tools)
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
 ;; https://github.com/raxod502/straight.el#the-recipe-format
